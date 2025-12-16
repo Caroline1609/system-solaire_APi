d432c27c86a2c78a5dda0bdb29d46a792c627a93
Caroline BIEHLER <70326049+Caroline1609@users.noreply.github.com>
2025-12-16 10:44:33 +0100

    upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$opts = [
+  'http'=> [
+    'method'=>"GET",
+    'header'=>"Accept-language: en\r\n" .
+              "Authorization: Bearer 00826525-ada6-4264-befe-79acc3d8693d\r\n",
+  ]
+];
+
+$context = stream_context_create($opts);
+
+// Accès à un fichier HTTP avec les entêtes HTTP indiqués ci-dessus
+$file = file_get_contents('https://api.le-systeme-solaire.net/rest/bodies/', false, $context);
+
+header("content-type: application/json");
+
+echo $file;
+
