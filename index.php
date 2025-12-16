61e3c6fc26cf8e1a126f17f4426ddaaae9d348a0
Caroline BIEHLER <70326049+Caroline1609@users.noreply.github.com>
2025-12-16 12:51:24 +0100

    upload

--- index.php
+++ index.php
@@ -1,18 +1,39 @@
 <?php
-$opts = [
-  'http'=> [
-    'method'=>"GET",
-    'header'=>"Accept-language: en\r\n" .
-              "Authorization: Bearer 00826525-ada6-4264-befe-79acc3d8693d\r\n",
-  ]
-];
+// $opts = [
+//   'http'=> [
+//     'method'=>"GET",
+//     'header'=>"Accept-language: en\r\n" .
+//               "Authorization: Bearer 00826525-ada6-4264-befe-79acc3d8693d\r\n",
+//   ]
+// ];
 
-$context = stream_context_create($opts);
+// $context = stream_context_create($opts);
 
-// Accès à un fichier HTTP avec les entêtes HTTP indiqués ci-dessus
-$file = file_get_contents('https://api.le-systeme-solaire.net/rest/bodies/', false, $context);
+// // Accès à un fichier HTTP avec les entêtes HTTP indiqués ci-dessus
+// $file = file_get_contents('https://api.le-systeme-solaire.net/rest/bodies/', false, $context);
 
-header("content-type: application/json");
+// header("content-type: application/json");
 
-echo $file;
 
+
+// echo $file;
+
+
+
+
+// $curl = curl_init('https://api.le-systeme-solaire.net/rest/bodies/');
+
+
+// curl_setopt($curl, CURLOPT_HTTPHEADER, [
+//     'Authorization: Bearer ' . "00826525-ada6-4264-befe-79acc3d8693d",
+// ]);
+
+// header("content-type: application/json");
+
+
+// curl_exec($curl);
+
+
+
+
+?>
